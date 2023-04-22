7bf36f914d918af43b5e1fc88940efdc642f4286
brian <blobo@redhat.com>
2023-04-21 23:04:32 -0300

    Se actualiza para la demo

--- backend/app.py
+++ backend/app.py
@@ -10,11 +10,13 @@ s3.config()
 app = Flask(__name__)
 
 
-@app.route('/static/theme')
-def theme():
+@app.route('/static/theme/<file_name>')
+def theme(file_name: str):
+    
     s3.prepare_static_files()
+    
     client = os.getenv('CLIENT')
-    return send_from_directory('static', f'{client}/style.css')
+    return send_from_directory('static', f'{client}/{file_name}')
 
 
 if __name__ == "__main__":
