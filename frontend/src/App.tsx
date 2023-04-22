4e003a1a57ba8f93645e8ce367092b8bd4d00ce5
brian <blobo@redhat.com>
2023-04-21 22:02:14 -0300

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>My first styled page</h1>
+
+        <h2>Welcome to my styled page!</h2>
+
+        <h3>It lacks images, but at least it has style.
+          And it has links, even if they don't go
+          anywhere&hellip;</h3>
+
+        <p>There should be more here, but I don't know
+          what yet.</p>
+
+        <address>Made 5 April 2004
+          by myself.</address>
+      </header>
+    </div>
+  );
+}
+
+export default App;
